e8ce5c82963693b341c620cb4c6176e792c42c06
Mohamed Hassan <pencilpix11@gmail.com>
2015-08-01 14:32:10 +0200

    ADD: helper classes v0.0.1

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,6 @@ var gulp = require('gulp');
 
 // to generate the plugin
 gulp.task('default', function(){
-    return gulp.src('./app/*.scss')
+    return gulp.src('./app/**/*.scss')
         .pipe(gulp.dest('./dist/'));
 });
