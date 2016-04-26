6c436070273e40d38fba84a218df4e1cdbbb0f8f
Mohamed hassan <pencilpix11@gmail.com>
2016-04-26 14:44:14 +0200

    remove the old structure

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-// including Gulp
-var gulp = require('gulp');
-
-// to generate the plugin
-gulp.task('default', function(){
-    return gulp.src('./app/**/*.scss')
-        .pipe(gulp.dest('./dist/'));
-});
